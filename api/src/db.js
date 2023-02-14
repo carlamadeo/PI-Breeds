cfc13c0f2f9c8c3f20d84a17fd21901dd9c923eb
carlamadeo <carlamadeo@gmail.com>
2023-02-14 18:36:46 -0300

    deploy

--- api/src/db.js
+++ api/src/db.js
@@ -8,15 +8,15 @@ const {
 } = process.env;
 
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
+
+const sequelize = new Sequelize(DB_PROD, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
-
-//const sequelize = new Sequelize(DB_PROD, {
-//  logging: false, // set to console.log to see the raw SQL queries
-//  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-//});
 
 const basename = path.basename(__filename);
 
